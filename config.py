f68f183cfd2c835d590458c808f247b312e3ba17
Eren Yeager <theerenyeagerx@gmail.com>
2025-08-23 16:51:27 +0530

    By @TheErenYeagerx

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = ("API_ID",)
+API_HASH = ("API_HASH")
+BOT_TOKEN = ("BOT_TOKEN")
+
+# This is optional if you want a fallback RTMP URL
+DEFAULT_RTMP_URL = ("rtmps://dc5-1.rtmp.t.me/s/")
